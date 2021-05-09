1537195a69d70038f0e045dbe31538c113234eb8
CosmicOdeum <59748847+OdeumBiomes@users.noreply.github.com>
2021-05-09 15:18:41 +0200

    sorrow gives catharsis(probably)

---
+++ Source/FF_Drugs/Mental States.cs
@@ -0,0 +1,29 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using Verse.AI;
+
+namespace FF_Drugs
+{
+	public class Freefolk_WanderSad : MentalState
+	{
+		public override RandomSocialMode SocialModeMax()
+		{
+			return RandomSocialMode.Off;
+		}
+		public override void MentalStateTick()
+		{
+		}
+		public override void PreStart()
+		{
+			base.PreStart();
+			Log.Error("Is it caused by a mood? " + causedByMood);
+			causedByMood = true;
+			Log.Error("Is it caused by a mood now? " + causedByMood);
+		}
+	}
+}
